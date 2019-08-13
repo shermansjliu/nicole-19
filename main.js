8f598e9073748586dff4e98de7e47d1d51b35087
Sherman Liu <shermanliu03@gmail.com>
2019-08-14 01:39:04 +0800

    created js file

---
+++ main.js
@@ -0,0 +1,25 @@
+let this_is_a_way = "Away We Happened. Who could forget. Even though this was released in 2012. Our friendship was ignited from the sparks of Wong fu and a very very enjoyable Geo Project.";
+let missing_piece = "Also featured in Away We Happened. This is by far my favorite song, but I know you prefer 'by my side' Grrr";
+let everything = "";
+let have_not_met_you = "";
+let by_my_side = ""
+let bygone_years = ""
+let thinking_about_you = ""
+let wang_leehom = "";
+let better_with_you = "";
+let sam_smith = "";
+let best_friend = "";
+let love_yourself_beautiful_wild = "";
+let my_time_with_you = ""
+let all_my_life = ""
+let dancing_in_moonlight = "";
+let love_is_an_open_door = "";
+let first_day_of_life = "";
+let sunday_morning = ""
+let by_chance_you_and_i = ""
+
+const songs = {
+    this_is_a_way: this_is_a_way,
+    missing_piece: missing_piece
+
+}
