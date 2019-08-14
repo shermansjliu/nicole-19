bbeb2c793e2aee71883163ac9c9d569de37fcb5c
Sherman Liu <shermanliu03@gmail.com>
2019-08-14 16:20:13 +0800

    completed showing song description feature

--- main.js
+++ main.js
@@ -18,8 +18,35 @@ let first_day_of_life = "";
 let sunday_morning = ""
 let by_chance_you_and_i = ""
 
+
 const songs = {
     this_is_a_way: this_is_a_way,
-    missing_piece: missing_piece
+    missing_piece: missing_piece,
+    everything: everything,
+    have_not_met_you: have_not_met_you,
+    by_my_side: by_my_side,
+    those_bygone_years: bygone_years,
+
 
 }
+
+
+
+const description_box = document.querySelector('.description-box');
+const back_button = document.querySelector('.back-button');
+let song_btns = document.querySelectorAll('.song')
+song_btns = [...song_btns]
+
+song_btns.forEach((btn)=> {
+    btn.addEventListener('click', (e)=>{
+        song_title = e.target.className.split(' ')[1];
+        console.log(song_title)
+        message = songs[song_title]
+        let new_p = document.createElement("p")
+        new_p.textContent = songs[song_title]
+        description_box.appendChild(new_p)
+        description_box.classList.toggle('show')
+        back_button.classList.toggle('show')
+        new_p.classList.toggle('description-text')
+    })
+})
